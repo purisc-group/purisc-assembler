d2e60b9159ea43722ecab4f5eb2d2d968e4dadaa
Josh Sammut <joshsammut@gmail.com>
2015-03-08 17:04:26 -0400

    Fixed issue with offset and NEXT

--- assembler.py
+++ assembler.py
@@ -18,11 +18,11 @@ def main(argv):
     inputFileName = "input.slq";
     formatMachineCode = False;
     verbose = False;
-    formatAsBinary = True;
+    formatAsBinary = False;
 
 #Command line arguments
     try:
-        opts, args = getopt.getopt(argv, "i:o:d:p:rfv")
+        opts, args = getopt.getopt(argv, "i:o:d:p:bfv")
     except getopt.GetoptError:
         print "Usage: python",sys.argv[0],"[-i inputfile] [-o outputfile] [-d dataMemoryOffset] [-p programMemoryOffset] [-f] [-v] [-r]"
         sys.exit(2);
@@ -38,10 +38,10 @@ def main(argv):
             dataMemOffset = int(arg);
 
         elif opt in ("-p", "--programOffset"):
-            programMemOffset = arg;
+            programMemOffset = int(arg);
         
-        elif opt in ("-r", "--readable"):
-            formatAsBinary = False;
+        elif opt in ("-b", "--binary"):
+            formatAsBinary = True;
                 
         elif opt == "-f":
             formatMachineCode = True;
@@ -59,13 +59,6 @@ def main(argv):
     programMemString = memoryArray[0];
     dataMemString = memoryArray[1];
 
-#create initial program memory
-    programMem = re.findall("\S+:\s*#?\S+|#?\S+|NEXT", programMemString); 
-
-    if len(programMem) > dataMemOffset:
-        print "Warning:\n Initial data memory location collides with program memory, setting initial data memory to: " + len(programMem)
-        dataMemOffset = len(programMem);
-
 #create initial data memory        
     nextDataMem = dataMemOffset;
     rawDataStrings = re.findall("\S+:\s*#[-1]?\d*", dataMemString);
@@ -82,14 +75,28 @@ def main(argv):
             while isReservedKeyword(nextDataMem):
                 nextDataMem += 1;
 
+
+#create initial program memory
+    programMem = re.findall("\S+:\s*#?[^\s,]+|#?[^\s,]+|NEXT", programMemString); 
+
+    if len(programMem) > dataMemOffset:
+        print "Warning:\n Initial data memory location collides with program memory, setting initial data memory to: " + len(programMem)
+        dataMemOffset = len(programMem);
+
 #resolve labels in program memory
     for i,val in enumerate(programMem):
+        print i
 
         if re.match("\S+:", val):
-            label = re.findall("\S+(?=:)", val)[0];
+            #get the label from the operand
+            label = re.findall("\S+(?=:)", val)[0]; 
+            
+            #remove the label and whitespace
             programMem[i] = re.findall("(?<=:)\s*\S+", val)[0];
             programMem[i] = re.sub("\s*","",programMem[i]);
 
+            #iterate through the program memory, replace the label with the appropriate line 
+            #address
             for j,value in enumerate(programMem):
                 if value == label:
                     programMem[j] = "#" + str(i + programMemOffset);
@@ -98,7 +105,7 @@ def main(argv):
     for i,val in enumerate(programMem):
             
         if val == "NEXT":
-            programMem[i] = "#" + str(i+1);            
+            programMem[i] = "#" + str(i+1+programMemOffset);            
   
 #resolve variables into addresses
     for i,val in enumerate(programMem):
@@ -115,7 +122,6 @@ def main(argv):
                 if re.match("#",offset): #literal
                     programMem[i] = str(dataMem[base][0] + int(offset[1:]));
                 else:
-                    print "no offset"
                     programMem[i] = str(dataMem[base][0] + dataMem[offset][0]);
 
 
@@ -124,11 +130,10 @@ def main(argv):
             programMem[i] = val[1:];
 
         else:
-            print val
             dataMem[val] = [nextDataMem, "0"]; #initialize data to 0
             programMem[i] = str(nextDataMem);                        
             nextDataMem += 1;
-  
+
 
 #output results
     outputFile = open(outputFileNames[0], "w");
@@ -181,11 +186,17 @@ def main(argv):
         value = dataMem[key][1];
         sortedDataMem[address-dataMemOffset] = [address, value];
 
-    for address in range(sortedDataMem[0][0], sortedDataMem[len(sortedDataMem)-1][0]+1):
-        if len(sortedDataMem[address-dataMemOffset]) == 2:
-            value = sortedDataMem[address-dataMemOffset][1];
+    #walk through datamem and output
+    count = 0;
+    for entry in sortedDataMem:
+        if len(entry) == 2:
+            address = entry[0];
+            value = entry[1];
         else:
-            value = 0
+            value = 0;
+            address = dataMemOffset + count;
+
+        count += 1;
             
         if(len(outputFileNames) == 1):
             outputFile.write(formatValue(value,formatAsBinary));
